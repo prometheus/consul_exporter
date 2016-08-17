05930011f30edc73a5f307893317236deb53ea34
Dylan Clendenin <dylan@betterdoctor.com>
2016-08-17 14:54:04 -0700

    use scheme if provided to -consul.server flag

---
+++ consul_exporter_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestNewExporter(t *testing.T) {
+	cases := []struct {
+		uri string
+		ok  bool
+	}{
+		{uri: "", ok: false},
+		{uri: "localhost:8500", ok: true},
+		{uri: "https://localhost:8500", ok: true},
+		{uri: "http://some.where:8500", ok: true},
+		{uri: "fuuuu://localhost:8500", ok: false},
+	}
+
+	for _, test := range cases {
+		_, err := NewExporter(test.uri, "", ".*", true)
+		if test.ok && err != nil {
+			t.Errorf("expected no error w/ %s but got %s", test.uri, err)
+		}
+		if !test.ok && err == nil {
+			t.Errorf("expected error w/ %s but got %s", test.uri, err)
+		}
+	}
+}
