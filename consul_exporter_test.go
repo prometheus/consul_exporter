a113bf8c0a1843faec194fd9eca8c861f89e145d
Simon Pasquier <spasquie@redhat.com>
2019-09-27 15:56:53 +0200

    Fix label clashes
    Signed-off-by: Simon Pasquier <spasquie@redhat.com>

--- consul_exporter_test.go
+++ consul_exporter_test.go
@@ -15,12 +15,13 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"os"
 	"testing"
+	"text/template"
 	"time"
 
+	consul_api "github.com/hashicorp/consul/api"
 	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/prometheus/common/expfmt"
 )
@@ -58,23 +59,25 @@ func TestCollect(t *testing.T) {
 		t.Skipf("CONSUL_NODE_NAME environment variable not set")
 	}
 
-	exporter, err := NewExporter(consulOpts{uri: addr, timeout: time.Duration(time.Second)}, "", "", true)
-	if err != nil {
-		t.Errorf("expected no error but got %q", err)
-	}
-
-	metrics := `# HELP consul_catalog_service_node_healthy Is this service healthy on this node?
+	for _, tc := range []struct {
+		name     string
+		metrics  string
+		services []*consul_api.AgentServiceRegistration
+	}{
+		{
+			name: "simple collect",
+			metrics: `# HELP consul_catalog_service_node_healthy Is this service healthy on this node?
 # TYPE consul_catalog_service_node_healthy gauge
-consul_catalog_service_node_healthy{node="%s",service_id="consul",service_name="consul"} 1
+consul_catalog_service_node_healthy{node="{{ .Node }}",service_id="consul",service_name="consul"} 1
 # HELP consul_catalog_services How many services are in the cluster.
 # TYPE consul_catalog_services gauge
 consul_catalog_services 1
 # HELP consul_health_node_status Status of health checks associated with a node.
 # TYPE consul_health_node_status gauge
-consul_health_node_status{check="serfHealth",node="%s",status="critical"} 0
-consul_health_node_status{check="serfHealth",node="%s",status="maintenance"} 0
-consul_health_node_status{check="serfHealth",node="%s",status="passing"} 1
-consul_health_node_status{check="serfHealth",node="%s",status="warning"} 0
+consul_health_node_status{check="serfHealth",node="{{ .Node }}",status="critical"} 0
+consul_health_node_status{check="serfHealth",node="{{ .Node }}",status="maintenance"} 0
+consul_health_node_status{check="serfHealth",node="{{ .Node }}",status="passing"} 1
+consul_health_node_status{check="serfHealth",node="{{ .Node }}",status="warning"} 0
 # HELP consul_raft_leader Does Raft cluster have a leader (according to this node).
 # TYPE consul_raft_leader gauge
 consul_raft_leader 1
@@ -83,32 +86,110 @@ consul_raft_leader 1
 consul_raft_peers 1
 # HELP consul_serf_lan_member_status Status of member in the cluster. 1=Alive, 2=Leaving, 3=Left, 4=Failed.
 # TYPE consul_serf_lan_member_status gauge
-consul_serf_lan_member_status{member="%s"} 1
+consul_serf_lan_member_status{member="{{ .Node }}"} 1
 # HELP consul_serf_lan_members How many members are in the cluster.
 # TYPE consul_serf_lan_members gauge
 consul_serf_lan_members 1
 # HELP consul_up Was the last query of Consul successful.
 # TYPE consul_up gauge
 consul_up 1
-`
-	expected := bytes.NewReader([]byte(fmt.Sprintf(metrics, node, node, node, node, node, node)))
+`,
+		},
+		{
+			name: "collect with duplicate tag values",
+			metrics: `# HELP consul_catalog_service_node_healthy Is this service healthy on this node?
+# TYPE consul_catalog_service_node_healthy gauge
+consul_catalog_service_node_healthy{node="{{ .Node }}",service_id="consul",service_name="consul"} 1
+consul_catalog_service_node_healthy{node="{{ .Node }}",service_id="foo",service_name="foo"} 1
+# HELP consul_catalog_services How many services are in the cluster.
+# TYPE consul_catalog_services gauge
+consul_catalog_services 2
+# HELP consul_service_tag Tags of a service.
+# TYPE consul_service_tag gauge
+consul_service_tag{node="{{ .Node }}",service_id="foo",tag="tag1"} 1
+consul_service_tag{node="{{ .Node }}",service_id="foo",tag="tag2"} 1
+`,
+			services: []*consul_api.AgentServiceRegistration{
+				&consul_api.AgentServiceRegistration{
+					ID:   "foo",
+					Name: "foo",
+					Tags: []string{"tag1", "tag2", "tag1"},
+				},
+			},
+		},
+		{
+			name: "collect with forward slash service name",
+			metrics: `# HELP consul_catalog_service_node_healthy Is this service healthy on this node?
+# TYPE consul_catalog_service_node_healthy gauge
+consul_catalog_service_node_healthy{node="{{ .Node }}",service_id="bar",service_name="bar"} 1
+consul_catalog_service_node_healthy{node="{{ .Node }}",service_id="consul",service_name="consul"} 1
+# HELP consul_catalog_services How many services are in the cluster.
+# TYPE consul_catalog_services gauge
+consul_catalog_services 3
+`,
+			services: []*consul_api.AgentServiceRegistration{
+				&consul_api.AgentServiceRegistration{
+					ID:   "slashbar",
+					Name: "/bar",
+				},
+				&consul_api.AgentServiceRegistration{
+					ID:   "bar",
+					Name: "bar",
+				},
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			exporter, err := NewExporter(consulOpts{uri: addr, timeout: time.Duration(time.Second)}, "", "", true)
+			if err != nil {
+				t.Errorf("expected no error but got %q", err)
+			}
 
-	// Only check metrics that are explicitly listed above.
-	var (
-		tp          expfmt.TextParser
-		metricNames []string
-	)
-	mfs, err := tp.TextToMetricFamilies(expected)
-	if err != nil {
-		t.Errorf("expected no error but got %q", err)
-	}
-	for _, mf := range mfs {
-		metricNames = append(metricNames, mf.GetName())
-	}
+			defer func() {
+				for _, svc := range tc.services {
+					err := exporter.client.Agent().ServiceDeregister(svc.ID)
+					if err != nil {
+						t.Logf("deregistering service %q: %v", svc.Name, err)
+					}
+				}
+			}()
+			for _, svc := range tc.services {
+				err := exporter.client.Agent().ServiceRegister(svc)
+				if err != nil {
+					t.Errorf("expected no error but got %s", err)
+				}
+			}
 
-	expected.Seek(0, io.SeekStart)
-	err = testutil.CollectAndCompare(exporter, expected, metricNames...)
-	if err != nil {
-		t.Errorf("expected no error but got %s", err)
+			tmpl, err := template.New(tc.name).Parse(tc.metrics)
+			if err != nil {
+				t.Errorf("expected no error but got %s", err)
+			}
+
+			var w bytes.Buffer
+			err = tmpl.Execute(&w, map[string]string{"Node": node})
+			if err != nil {
+				t.Errorf("expected no error but got %s", err)
+			}
+
+			// Only check metrics that are explicitly listed above.
+			var (
+				tp          expfmt.TextParser
+				metricNames []string
+				buf         = bytes.NewReader(w.Bytes())
+			)
+			mfs, err := tp.TextToMetricFamilies(buf)
+			if err != nil {
+				t.Errorf("expected no error but got %q", err)
+			}
+			for _, mf := range mfs {
+				metricNames = append(metricNames, mf.GetName())
+			}
+
+			buf.Seek(0, io.SeekStart)
+			err = testutil.CollectAndCompare(exporter, buf, metricNames...)
+			if err != nil {
+				t.Errorf("expected no error but got %s", err)
+			}
+		})
 	}
 }
