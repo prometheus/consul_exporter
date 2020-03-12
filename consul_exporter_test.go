d194d722cbe4adb3e1321b6c7f1e5ebfa0de7bd3
Andrew Cornies <acornies@gmail.com>
2020-03-12 14:58:51 -0400

    Update readme, attempt to fix check
    Signed-off-by: Andrew Cornies <acornies@gmail.com>

--- consul_exporter_test.go
+++ consul_exporter_test.go
@@ -141,12 +141,9 @@ consul_catalog_services 3
 		},
 		{
 			name: "collect with service check name",
-			metrics: `# HELP consul_catalog_service_node_healthy Is this service healthy on this node?
+			metrics: `# HELP consul_service_checks Is there a service with a check name?
 # TYPE consul_service_checks gauge
 consul_service_checks{service_id="special",service_name="special",check_id="_nomad-check-special",check_name="friendly-name"} 1
-# HELP consul_service_checks How many services are in the cluster.
-# TYPE consul_catalog_services gauge
-consul_service_checks 1
 `,
 			services: []*consul_api.AgentServiceRegistration{
 				&consul_api.AgentServiceRegistration{
